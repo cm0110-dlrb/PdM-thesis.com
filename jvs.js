ef8322c5103631118fd7230f40899119c8d4eb6a
cm0110-dlrb <145824652+cm0110-dlrb@users.noreply.github.com>
2023-12-09 17:31:40 +0800

    Update jvs.js

--- jvs.js
+++ jvs.js
@@ -1,4 +1,4 @@
-const dbx = new Dropbox({ accessToken: 'sl.BrZ6SzcRrPHNFkyIBtySnefZwgxkrMYB0EGYzy8iMFOq8dgK5MllkAbHhCDAVCjdifbJo5CzIcb_hDT_UP8SqqnuqE_WVtn9bWaP9M_RzMairhtpKmWXkddJaXBQevM0TvElEgfUHoqQxRH1ugcG' });
+const dbx = process.env.sl.BrZ6SzcRrPHNFkyIBtySnefZwgxkrMYB0EGYzy8iMFOq8dgK5MllkAbHhCDAVCjdifbJo5CzIcb_hDT_UP8SqqnuqE_WVtn9bWaP9M_RzMairhtpKmWXkddJaXBQevM0TvElEgfUHoqQxRH1ugcG;
 dbx.usersGetCurrentAccount()
  .then(response => {
     console.log(response);
